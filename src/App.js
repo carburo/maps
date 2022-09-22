f553180b7b42d47374554d0ff5ddbb771322f2f2
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2022-09-22 11:28:41 -0400

    add maps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import OpenStreetMap from "./demos/OpenStreetMap";
+import Vector from "./demos/Vector";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Open Street Map</h2>
+      <OpenStreetMap />
+      <h2>Vectorial</h2>
+      <Vector />
     </div>
   );
 }
---
+++ src/demos/Vector.jsx
@@ -0,0 +1,35 @@
+// import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
+import { MapContainer, Marker, Popup, useMap } from "react-leaflet";
+import leaflet from "leaflet";
+import data from "./usa.json";
+
+const position = [51.505, -0.09];
+
+export default function Vector() {
+  return (
+    <MapContainer
+      style={{ height: 400 }}
+      center={position}
+      zoom={2}
+      scrollWheelZoom={false}
+    >
+      <VectorMap />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+function VectorMap() {
+  const map = useMap();
+  useEffect(() => {
+    const jsonMap = leaflet.geoJson(data);
+    map.addLayer(jsonMap);
+  }, [map]);
+
+  return null;
+}
