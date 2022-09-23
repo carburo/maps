ac10f0939a784ace47582f687aa05ee0d6c0aff5
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2022-09-22 20:51:39 -0400

    change marker position on click

--- src/demos/Vector.jsx
+++ src/demos/Vector.jsx
@@ -1,22 +1,40 @@
-import { MapContainer, Marker, Popup, GeoJSON } from "react-leaflet";
+import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  GeoJSON,
+  useMapEvent,
+} from "react-leaflet";
 import data from "./usa.json";
 
-const position = [51.505, -0.09];
+const origin = [51.505, -0.09];
 
 export default function Vector() {
   return (
     <MapContainer
       style={{ height: "100vh" }}
-      center={position}
+      center={origin}
       zoom={2}
       scrollWheelZoom={false}
     >
       <GeoJSON data={data} />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <InteractiveMarker />
     </MapContainer>
+  );
+}
+
+function InteractiveMarker() {
+  const [position, setPosition] = useState(origin);
+  useMapEvent("click", (event) => {
+    setPosition(event.latlng);
+  });
+
+  return (
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
   );
 }
